eea9b80542f0ec54d315f91c6c1e451031ac8402
Vincenzo Calendini <vincenzocalendini@gmail.com>
2021-12-13 12:09:31 +0100

    added script for create a prestshop module

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,6 @@
 rsync -av --progress --exclude="node_modules" --exclude=“202” --exclude=".git" --exclude="package-lock.json" --exclude="composer.lock" ./ ./stripe_official_patched
 cd ./stripe_official_patched
-# zip -r stripe.module.zip ./
-# cd ../
-# rm -rf ./stripe_official_patched
+zip -r stripe.module.zip ./
+mv stripe.module.zip ../
+cd ../
+rm -rf ./stripe_official_patched
