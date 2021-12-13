baa86c9a723b3d7debfcce6484518c8bfa951a76
Vincenzo Calendini <vincenzocalendini@gmail.com>
2021-12-13 12:06:29 +0100

    update

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+rsync -av --progress --exclude="node_modules" --exclude=“202” --exclude=".git" --exclude="package-lock.json" --exclude="composer.lock" ./ ./stripe_official_patched
+cd ./stripe_official_patched
+# zip -r stripe.module.zip ./
+# cd ../
+# rm -rf ./stripe_official_patched
